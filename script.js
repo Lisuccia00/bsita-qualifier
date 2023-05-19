dfb96fffcc4d1e8be014688f51aa73abe7a10fa8
Lisuccia00 <l.manoni1@campus.unimib.it>
2023-05-19 15:18:49 +0200

    pizzi aggiunto
    feaafe

--- script.js
+++ script.js
@@ -9,11 +9,21 @@ async function getData(){
         var response = await fetch(responseurl)
         var map_details = await fetch(mapdetailsurl)
         var {scores} = await response.json()
+        var pizziscore = await fetch(pizziurl)
+        if(pizziscore.ok){
+            var pizzi = await pizziscore.json()
+            pizzi = pizzi.scores
+            pizzi.forEach(element => {
+                scores.push(element)
+            });
+        }
+        scores.sort((a,b) => (a.baseScore > b.baseScore) ? -1:1)
         map_details = await map_details.json()
         var {maxScore} = map_details
         var songName = map_details.songName + ' - '+ map_details.songAuthorName
         createNewTable(songName, map)
-        var ID = []
+        ID = []
+        var rank = []
         var pfpUrl = []
         var name = []
         var perc = []
@@ -21,24 +31,24 @@ async function getData(){
         var totalpoints = []
         for(var j = 0; j < scores.length; j++){
             var points
-            if (scores[j].rank < 3){
+            if (j + 1 < 3){
                 points = 1000
-            }else if(scores[j].rank == 3){
+            }else if(j + 1 == 3){
                 points = 100
-            }else if(scores[j].rank == 4){
+            }else if(j + 1 == 4){
                 points = 10
             }else{
                 points = 1
             }
-            ID[j] = scores[j].rank
+            ID[j] = scores[j].leaderboardPlayerInfo.id
+            rank[j] = j + 1
             pfpUrl[j] = scores[j].leaderboardPlayerInfo.profilePicture
             name[j] = scores[j].leaderboardPlayerInfo.name
             perc[j] = ((scores[j].baseScore / maxScore)* 100).toFixed(2)
             miss[j] = scores[j].missedNotes
             totalpoints[j] = points
         }
-        console.log(pfpUrl)
-        populate(ID, pfpUrl ,  name, perc, miss, totalpoints, map)
+        populate(ID,rank,  pfpUrl ,  name, perc, miss, totalpoints, map)
     }
 }
 
@@ -47,20 +57,22 @@ function createNewTable(songname, id){
     table.innerHTML += '<table id="'+ id +'"><caption>'+ songname + '<thead><tr><th class="first center">#</th class="center"><th></th><th>name</th><th>score</th><th class="lastMobile center">miss</th><th class="last mobileHide center">points</th></tr></thead><tbody id="body"></tbody></table>'
 }
 
-function populate(ID, pfp, name, perc, miss, points, map_id){
+function populate(id, rank, pfp, name, perc, miss, points, map_id){
     var table = document.getElementById(map_id).childNodes[2]
     
-    for(var i = 0; i < ID.length; i++){
+    for(var i = 0; i < rank.length; i++){
         var row = document.createElement('tr')
-        var IDcell = document.createElement('td')
-        IDcell.classList.add("center")
+        var rankcell = document.createElement('td')
+        rankcell.classList.add("center")
         if(i < 3){
-            IDcell.classList.add("green")
+            rankcell.classList.add("green")
         }else if(i > 3){
-            IDcell.classList.add("red")
+            rankcell.classList.add("red")
         }
         var namecell = document.createElement('td')
         namecell.classList.add("name")
+        var nameLink = document.createElement('a')
+        nameLink.href = "https://scoresaber.com/u/" + id[i]
         var pfpcell = document.createElement('td')
         var pfpurl = document.createElement('img')
         pfpurl.src = pfp[i]
@@ -71,13 +83,14 @@ function populate(ID, pfp, name, perc, miss, points, map_id){
         var pointscell = document.createElement('td')
         pointscell.classList.add("mobileHide" , "center")
         var dummy
-        IDcell.appendChild(dummy = document.createTextNode(ID[i]))
+        rankcell.appendChild(dummy = document.createTextNode(rank[i]))
         pfpcell.appendChild(pfpurl)
-        namecell.appendChild(dummy = document.createTextNode(name[i]))
+        nameLink.appendChild(dummy = document.createTextNode(name[i]))
+        namecell.appendChild(nameLink)
         perccell.appendChild(dummy = document.createTextNode(perc[i]))
         misscell.appendChild(dummy = document.createTextNode(miss[i]))
         pointscell.appendChild(dummy = document.createTextNode(points[i]))
-        row.appendChild(IDcell)
+        row.appendChild(rankcell)
         row.appendChild(pfpcell)
         row.appendChild(namecell)
         row.appendChild(perccell)
