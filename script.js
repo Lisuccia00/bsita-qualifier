6240fdc62495faa114bab942d12f1d3c04a91f1f
Lisuccia00 <l.manoni1@campus.unimib.it>
2023-05-19 13:57:24 +0200

    center
    akengfkae

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ async function getData(){
         var songName = map_details.songName + ' - '+ map_details.songAuthorName
         createNewTable(songName, map)
         var ID = []
+        var pfpUrl = []
         var name = []
         var perc = []
         var miss = []
@@ -30,37 +31,49 @@ async function getData(){
                 points = 1
             }
             ID[j] = scores[j].rank
+            pfpUrl[j] = scores[j].leaderboardPlayerInfo.profilePicture
             name[j] = scores[j].leaderboardPlayerInfo.name
             perc[j] = ((scores[j].baseScore / maxScore)* 100).toFixed(2)
             miss[j] = scores[j].missedNotes
             totalpoints[j] = points
         }
-        populate(ID, name, perc, miss, totalpoints, map)
+        console.log(pfpUrl)
+        populate(ID, pfpUrl ,  name, perc, miss, totalpoints, map)
     }
 }
 
 function createNewTable(songname, id){
     var table = document.getElementById('container')
-    table.innerHTML += '<table id="'+ id +'"><caption>'+ songname + '<thead><tr><th class="first">#</th><th>name</th><th>score</th><th>miss</th><th class="last">points</th></tr></thead><tbody id="body"></tbody></table>'
+    table.innerHTML += '<table id="'+ id +'"><caption>'+ songname + '<thead><tr><th class="first center">#</th class="center"><th></th><th>name</th><th>score</th><th class="lastMobile center">miss</th><th class="last mobileHide center">points</th></tr></thead><tbody id="body"></tbody></table>'
 }
 
-function populate(ID, name, perc, miss, points, map_id){
+function populate(ID, pfp, name, perc, miss, points, map_id){
     var table = document.getElementById(map_id).childNodes[2]
     
     for(var i = 0; i < ID.length; i++){
         var row = document.createElement('tr')
         var IDcell = document.createElement('td')
+        IDcell.classList.add("center")
         var namecell = document.createElement('td')
+        namecell.classList.add("name")
+        var pfpcell = document.createElement('td')
+        var pfpurl = document.createElement('img')
+        pfpurl.src = pfp[i]
+        pfpurl.classList.add("pfp", "center")
         var perccell = document.createElement('td')
         var misscell = document.createElement('td')
+        misscell.classList.add("center")
         var pointscell = document.createElement('td')
+        pointscell.classList.add("mobileHide" , "center")
         var dummy
         IDcell.appendChild(dummy = document.createTextNode(ID[i]))
+        pfpcell.appendChild(pfpurl)
         namecell.appendChild(dummy = document.createTextNode(name[i]))
         perccell.appendChild(dummy = document.createTextNode(perc[i]))
         misscell.appendChild(dummy = document.createTextNode(miss[i]))
         pointscell.appendChild(dummy = document.createTextNode(points[i]))
         row.appendChild(IDcell)
+        row.appendChild(pfpcell)
         row.appendChild(namecell)
         row.appendChild(perccell)
         row.appendChild(misscell)
