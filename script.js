493211f9eb3469f098f738f58a0082888aa0b7e6
Lisuccia00 <l.manoni1@campus.unimib.it>
2023-05-21 19:43:19 +0200

    sdfkolpjgsiopg
    jssjtrjtr

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var map_id = ["544408", "66733", "555936", "532992", "557955", "555606", "546669"]
-
+var sortedArray = []
 async function getData(){
     var finalStandings = new Map()
+    var pfps = new Map()
     for(var i = 0; i < map_id.length; i++){
+        
         var map = map_id[i]
         var responseurl = 'https://corsproxy.io/?' + 'https://scoresaber.com/api/leaderboard/by-id/' + map + '/scores?countries=it&page=1'
         var pizziurl = 'https://corsproxy.io/?' + 'https://scoresaber.com/api/leaderboard/by-id/' + map + '/scores?countries=us&search=sionpizzi'
@@ -59,6 +61,7 @@ async function getData(){
                 perc[j] = ((scores[j].baseScore / maxScore)* 100).toFixed(2)
                 miss[j] = scores[j].missedNotes + scores[j].badCuts
                 totalpoints[j] = points
+
                 if (finalStandings.has(name[j])) {
                     const currentValue = finalStandings.get(name[j]);
                     finalStandings.set(name[j], currentValue + points);
@@ -67,18 +70,71 @@ async function getData(){
                     finalStandings.set(name[j], points);
                 }
 
+                if (pfps.has(name[j])) {
+                    pfps.set(name[j], pfpUrl[j])
+                } 
+                else {
+                    pfps.set(name[j], pfpUrl[j]);
+                }
             }
-            populate(ID,rank,  pfpUrl ,  name, perc, miss, totalpoints, map)
+            populate(ID, rank, pfpUrl, name, perc, miss, totalpoints, map)
         }
     }
-    const sortedArray = Array.from(finalStandings).sort((a, b) => b[1] - a[1])
-    console.log(sortedArray)
+    console.log(pfps)
+    sortedArray = Array.from(finalStandings).sort((a, b) => b[1] - a[1])
+    createOverallTable()
+    populateOverallTable(sortedArray, pfps)
 }
 
 function createNewTable(songname, id){
     var table = document.getElementById('container')
-    table.innerHTML += '<table id="'+ id +'"><caption>'+ songname + '<thead><tr><th class="first center">#</th class="center"><th></th><th>name</th><th>score</th><th class="lastMobile center">miss</th><th class="last mobileHide center">points</th></tr></thead><tbody id="body"></tbody></table>'
+    table.innerHTML += '<table id="'+ id +'" class="maps hidden animate"><caption>'+ songname + '<thead><tr><th class="first center">#</th class="center"><th></th><th>name</th><th>score</th><th class="lastMobile center">miss</th><th class="last mobileHide center">points</th></tr></thead><tbody id="body"></tbody></table>'
 }
+
+function createOverallTable(){
+    var table = document.getElementById('container')
+    table.innerHTML += '<table id= "overall"class="overall hidden animate"><caption>Overall Results<thead><tr><th class="first center">#</th class="center"><th></th><th>name</th><th class="last center">points</th></tr></thead><tbody id="body"></tbody></table>'
+}
+
+function populateOverallTable(namePoints, pfp){
+    console.log(pfp)
+    for(var i = 0; i < namePoints.length; i++){
+        var table = document.getElementById("overall").childNodes[2]
+        var name = namePoints[i][0]
+        var points = namePoints[i][1]
+        var pfpUrl = pfp.get(name)
+        console.log(pfpUrl)
+        var row = document.createElement('tr')
+        var rankcell = document.createElement('td')
+        rankcell.classList.add("center")
+        if(i < 8){
+            rankcell.classList.add("green")
+        }else{
+            rankcell.classList.add("red")
+        }
+        var namecell = document.createElement('td')
+        namecell.classList.add("name")
+        var pfpcell = document.createElement('td')
+        var pfpurl = document.createElement('img')
+        pfpurl.src = pfpUrl
+        pfpurl.classList.add("pfp", "center")
+        var pointscell = document.createElement('td')
+        pointscell.classList.add("center")
+        var dummy
+        rankcell.appendChild(dummy = document.createTextNode(i + 1))
+        pfpcell.appendChild(pfpurl)
+        namecell.appendChild(dummy = document.createTextNode(name))
+        pointscell.appendChild(dummy = document.createTextNode(points))
+        row.appendChild(rankcell)
+        row.appendChild(pfpcell)
+        row.appendChild(namecell)
+        row.appendChild(pointscell)
+        table.appendChild(row)
+    }
+    Show('overall')
+    Hide('loader')
+}
+
 
 function populate(id, rank, pfp, name, perc, miss, points, map_id){
     var table = document.getElementById(map_id).childNodes[2]
@@ -122,5 +178,28 @@ function populate(id, rank, pfp, name, perc, miss, points, map_id){
         row.appendChild(misscell)
         row.appendChild(pointscell)
         table.appendChild(row)
+    }
+}
+
+
+function Hide(src){
+    var elements = document.getElementsByClassName(src)
+    for(var i = 0; i < elements.length; i++){
+        elements[i].classList.add("hidden")
+    }
+}
+
+function Show(src){
+    var elements = document.getElementsByClassName(src)
+    for(var i = 0; i < elements.length; i++){
+        elements[i].classList.remove("hidden")
+    }
+}
+
+
+function Hamburger(){
+    const navbarLinks = document.getElementsByClassName('menus')
+    for(var i= 0; i< navbarLinks.length; i++){
+        navbarLinks[i].classList.toggle('active')
     }
 }
